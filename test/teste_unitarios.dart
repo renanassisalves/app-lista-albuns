ef64abaae70c404eec0418210c9d97a2848ae343
Renan de Assis Alves <renan.assis.alves@hotmail.com>
2023-06-22 22:19:00 -0300

    Ajustes nos testes para funcionar o Firebase no testes de widgets

--- test/teste_unitarios.dart
+++ test/teste_unitarios.dart
@@ -6,24 +6,62 @@ import 'package:entrega_flutter_2_package/entrega_flutter_2_package.dart';
 import 'package:entrega_flutter_2_package/models/album.dart';
 
 void main() {
+  int idAleatorio = Random().nextInt(100);
   group('Pacotes', () {
-    int idAleatorio = Random().nextInt(100);
     test('Testar se um álbum é gerado a partir da biblioteca de geração', () {
-      Album albumGerado = GeradorAlbuns().gerarAlbum(idAleatorio);
+      Album albumGerado = GeradorAlbuns.gerarAlbum(idAleatorio);
       expect(albumGerado, isA<Album>());
     });
 
     test('Testar se o id gerado é realmente implementado no álbum gerado', () {
-      Album albumGerado = GeradorAlbuns().gerarAlbum(idAleatorio);
+      Album albumGerado = GeradorAlbuns.gerarAlbum(idAleatorio);
       expect(albumGerado.id, idAleatorio);
     });
 
     test('Testar se a imagem gerada é uma imagem válida', () async {
-      Album albumGerado = GeradorAlbuns().gerarAlbum(idAleatorio);
+      Album albumGerado = GeradorAlbuns.gerarAlbum(idAleatorio);
       final response = await http.get(Uri.parse(albumGerado.thumbnailUrl));
       expect(response.statusCode, equals(200));
       String? contentType = response.headers['content-type'];
       expect(contentType, startsWith('image/'));
+    });
+  });
+  group('Testes models', () {
+    test('Testar se é criado um álbum através de um JSON', () {
+      final albumCriado = Album.fromJson({
+        "id": idAleatorio,
+        "title": "Álbum teste",
+        "url": "https://google.com.br",
+        "thumbnailUrl": "https://google.com.br/image"
+      });
+      expect(albumCriado, isA<Album>());
+    });
+    test('Testar se é criado uma lista albuns através de um JSON', () {
+      List<Album> albumCriado = Album.listFromJson([
+        {
+          "id": idAleatorio,
+          "title": "Álbum teste",
+          "url": "https://google.com.br",
+          "thumbnailUrl": "https://google.com.br/image"
+        },
+        {
+          "id": idAleatorio + 1,
+          "title": "Álbum teste 2",
+          "url": "https://google.com.br",
+          "thumbnailUrl": "https://google.com.br/image"
+        }
+      ]);
+      expect(albumCriado, isA<List<Album>>());
+    });
+    test('Testar se o favorito é alternado e setado', () {
+      Album albumTeste = Album(idAleatorio, "Album teste",
+          "https://google.com.br", "https://google.com.br");
+
+      expect(albumTeste.favorite, false);
+      albumTeste.toggleFavorite();
+      expect(albumTeste.favorite, true);
+      albumTeste.setFavorite(false);
+      expect(albumTeste.favorite, false);
     });
   });
 }
